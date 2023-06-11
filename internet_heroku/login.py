e53d1db0aab6d22fbdaa4e43e897553da5affecd
Juan Andrew <juandreww@outlook.com>
2023-06-12 06:58:46 +0700

    login PY

--- internet_heroku/login.py
+++ internet_heroku/login.py
@@ -1,20 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+
+def find_elements(driver):
+  username  = driver.find_element("id", "username")
+  password = driver.find_element("id", "password")
+  radius = driver.find_element(By.CLASS_NAME, "radius")
+
+  return username, password, radius
+
+def perform_first_task(username, password, radius):
+  username.send_keys("belajar_selenium@yahoo.com")
+  password.send_keys("belajar_itu_menyenangkan")
+  radius.click()
 
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/login")
 
-username  = driver.find_element("id", "username")
-username.send_keys("belajar_selenium@yahoo.com")
-
-password = driver.find_element("id", "password")
-password.send_keys("belajar_itu_menyenangkan")
+username, password, radius = find_elements(driver)
+perform_first_task(username, password, radius)
+time.sleep(3)
+perform_clear
 
 # elements (multiple)
 h2 = driver.find_elements(By.TAG_NAME, "h2")
 print(h2)
-
-driver.find_element(By.CLASS_NAME, "radius").click()
 
 # can also use By.LINK_TEXT
 # driver.find_element(By.PARTIAL_LINK_TEXT, "Elemental").click()
