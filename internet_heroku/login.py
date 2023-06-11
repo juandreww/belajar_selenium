d132f8e2d33220ac3029908ff14584ecd3870394
Juan Andrew <juandreww@outlook.com>
2023-06-11 22:00:31 +0700

    login py

--- internet_heroku/login.py
+++ internet_heroku/login.py
@@ -5,6 +5,7 @@ driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/login")
 driver.find_element("id", "username").send_keys("belajar_selenium@yahoo.com")
 driver.find_element("id", "password").send_keys("belajar_itu_menyenangkan")
+driver.find_element("link_text", "Elemental Selenium").click()
 
 # Wait for user input before closing the browser window
 input("Press Enter to quit")
