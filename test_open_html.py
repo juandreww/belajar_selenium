12650b09f1b1204d6f8882862167d2cbcbf1caba
Juan Andrew <juandreww@outlook.com>
2023-06-11 21:53:25 +0700

    test open html

--- test_open_html.py
+++ test_open_html.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("https://gooogle.com")
+driver.get("https://google.com")
 
 # Wait for user input before closing the browser window
 input("Press Enter to quit")
