f8a0c02b9dbce208106139ac288badf7139832ca
Juan Andrew <juandreww@outlook.com>
2023-06-12 06:52:34 +0700

    login PY

--- internet_heroku/login.py
+++ internet_heroku/login.py
@@ -1,20 +1,23 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-
 driver.get("https://the-internet.herokuapp.com/login")
-driver.find_element("id", "username").send_keys("belajar_selenium@yahoo.com")
-driver.find_element("id", "password").send_keys("belajar_itu_menyenangkan")
+
+username  = driver.find_element("id", "username")
+username.send_keys("belajar_selenium@yahoo.com")
+
+password = driver.find_element("id", "password")
+password.send_keys("belajar_itu_menyenangkan")
 
 # elements (multiple)
 h2 = driver.find_elements(By.TAG_NAME, "h2")
 print(h2)
 
-# can also use By.LINK_TEXT
-driver.find_element(By.PARTIAL_LINK_TEXT, "Elemental").click()
+driver.find_element(By.CLASS_NAME, "radius").click()
 
+# can also use By.LINK_TEXT
+# driver.find_element(By.PARTIAL_LINK_TEXT, "Elemental").click()
 
 # Wait for user input before closing the browser window
 input("Press Enter to quit")
