ffbbd58e0bee485fd46e550f2d37a172208bc89e
Juan Andrew <juandreww@outlook.com>
2023-06-13 07:08:32 +0700

    add remove element

--- internet_heroku/add_remove_element.py
+++ internet_heroku/add_remove_element.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
 driver.find_element(By.CSS_SELECTOR, "#content > div > button").click()
 time.sleep(5)
-driver.find_element(By.CSS_SELECTOR, "#elements > button").click()
+driver.find_element(By.XPATH, "//*[@id='elements']/button").click()
 
 input("Press Enter to quit")
 driver.quit()
